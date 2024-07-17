6a18dd47dab8dad29139273d5a21e883c3f0cfcc
armymp <67810944+armymp@users.noreply.github.com>
2024-07-17 18:26:52 -0400

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+    
+    private PlayerActions _actions;
+    private Rigidbody2D _rb2D;
+    private Vector2 _moveDirection;
+    private void Awake()
+    {
+        _actions = new PlayerActions();
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        ReadMovement();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Time.fixedDeltaTime to ignore framerate. Character moves in a fixed rate
+        _rb2D.MovePosition(_rb2D.position + _moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+    private void ReadMovement()
+    {
+        _moveDirection = _actions.Movement.Move.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnEnable()
+    {
+        _actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _actions.Disable();
+    }
+}
