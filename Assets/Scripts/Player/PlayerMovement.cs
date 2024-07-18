52f24ce2bbe294d93bd3e698c1ea15d791a913b9
armymp <67810944+armymp@users.noreply.github.com>
2024-07-17 20:08:24 -0400

    Add walk animation
    Add walk animations for up, down, left, and right movements, and add it to PlayerMovement.cs file

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,13 +7,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Config")]
     [SerializeField] private float speed;
-    
+
+    private readonly int _moveX = Animator.StringToHash("MoveX");
+    private readonly int _moveY = Animator.StringToHash("MoveY");
     private PlayerActions _actions;
     private Rigidbody2D _rb2D;
+    private Animator _animator;
     private Vector2 _moveDirection;
     private void Awake()
     {
         _actions = new PlayerActions();
+        _animator = GetComponent<Animator>();
         _rb2D = GetComponent<Rigidbody2D>();
     }
 
@@ -35,6 +39,9 @@ public class PlayerMovement : MonoBehaviour
     private void ReadMovement()
     {
         _moveDirection = _actions.Movement.Move.ReadValue<Vector2>().normalized;
+        if (_moveDirection == Vector2.zero) return;
+        _animator.SetFloat(_moveX, _moveDirection.x);
+        _animator.SetFloat(_moveY, _moveDirection.y);
     }
 
     private void OnEnable()
